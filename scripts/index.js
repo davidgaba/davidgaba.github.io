34edce7fba2321c2ffb940fddd829809d317d047
davidgaba <davidgaba03@gmail.com>
2024-05-08 12:03:23 -0700

    Added sliding toggle for color theme

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,14 @@
 "use strict";
 
-const $
+$("#color-theme-btn").click(colorThemeToggle);
+
+function colorThemeToggle() {
+  const $colorThemeBtn = $("#color-theme-btn");
+  const $colorThemeSlider = $("#color-theme-slider");
+
+  if ($colorThemeSlider.css("right") === "20.5px") {
+    $colorThemeSlider.css("right", "45px");
+  } else {
+    $colorThemeSlider.css("right", "20.5px");
+  }
+}
